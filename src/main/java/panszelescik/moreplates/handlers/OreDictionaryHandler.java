5e6898aeebd4da01814df33ab08455a1e005a016
PanSzelescik <pan.szelescik20010131@gmail.com>
2017-07-17 18:33:08 +0200

    More ore dictionary

---
+++ src/main/java/panszelescik/moreplates/handlers/OreDictionaryHandler.java
@@ -0,0 +1,54 @@
+package panszelescik.moreplates.handlers;
+
+import net.minecraftforge.oredict.OreDictionary;
+import panszelescik.moreplates.init.ModItems;
+
+public class OreDictionaryHandler {
+
+	public static void registerOreDictionary() {
+		
+		OreDictionary.registerOre("gearArdite", ModItems.arditeGear);
+		OreDictionary.registerOre("plateArdite", ModItems.arditePlate);
+		OreDictionary.registerOre("gearDraconiumAwakened", ModItems.awakenedDraconiumGear);
+		OreDictionary.registerOre("plateDraconiumAwakened", ModItems.awakenedDraconiumPlate);
+		OreDictionary.registerOre("gearCobalt", ModItems.cobaltGear);
+		OreDictionary.registerOre("plateCobalt", ModItems.cobaltPlate);
+		OreDictionary.registerOre("gearConductiveIron", ModItems.conductiveIronGear);
+		OreDictionary.registerOre("plateConductiveIron", ModItems.conductiveIronPlate);
+		OreDictionary.registerOre("gearDarkSteel", ModItems.darkSteelGear);
+		OreDictionary.registerOre("plateDarkSteel", ModItems.darkSteelPlate);
+		OreDictionary.registerOre("gearDraconium", ModItems.draconiumGear);
+		OreDictionary.registerOre("plateDraconium", ModItems.draconiumPlate);
+		OreDictionary.registerOre("gearElectricalSteel", ModItems.electricalSteelGear);
+		OreDictionary.registerOre("plateElectricalSteel", ModItems.electricalSteelPlate);
+		OreDictionary.registerOre("gearElementium", ModItems.elementiumGear);
+		OreDictionary.registerOre("plateElementium", ModItems.elementiumPlate);
+		OreDictionary.registerOre("gearEnergeticAlloy", ModItems.energeticAlloyGear);
+		OreDictionary.registerOre("plateEnergeticAlloy", ModItems.energeticAlloyPlate);
+		OreDictionary.registerOre("gearGaiaSpirit", ModItems.gaiaSpiritGear);
+		OreDictionary.registerOre("plateGaiaSpirit", ModItems.gaiaSpiritPlate);
+		OreDictionary.registerOre("gearKnightslime", ModItems.knightslimeGear);
+		OreDictionary.registerOre("plateKnightslime", ModItems.knightslimePlate);
+		OreDictionary.registerOre("gearManasteel", ModItems.manasteelGear);
+		OreDictionary.registerOre("plateManasteel", ModItems.manasteelPlate);
+		OreDictionary.registerOre("gearManyullyn", ModItems.manyullynGear);
+		OreDictionary.registerOre("plateManyullyn", ModItems.manyullynPlate);
+		OreDictionary.registerOre("gearOsmium", ModItems.osmiumGear);
+		OreDictionary.registerOre("plateOsmium", ModItems.osmiumPlate);
+		OreDictionary.registerOre("gearPigiron", ModItems.pigIronGear);
+		OreDictionary.registerOre("platePigiron", ModItems.pigIronPlate);
+		OreDictionary.registerOre("gearPulsatingIron", ModItems.pulsatingIronGear);
+		OreDictionary.registerOre("platePulsatingIron", ModItems.pulsatingIronPlate);
+		OreDictionary.registerOre("gearQuartzEnrichedIron", ModItems.quartzEnrichedIronGear);
+		OreDictionary.registerOre("plateQuartzEnrichedIron", ModItems.quartzEnrichedIronPlate);
+		OreDictionary.registerOre("gearRedstoneAlloy", ModItems.redstoneAlloyGear);
+		OreDictionary.registerOre("plateRedstoneAlloy", ModItems.redstoneAlloyPlate);
+		OreDictionary.registerOre("gearSoularium", ModItems.soulariumGear);
+		OreDictionary.registerOre("plateSoularium", ModItems.soulariumPlate);
+		OreDictionary.registerOre("gearTerrasteel", ModItems.terrasteelGear);
+		OreDictionary.registerOre("plateTerrasteel", ModItems.terrasteelPlate);
+		OreDictionary.registerOre("gearVibrantAlloy", ModItems.vibrantAlloyGear);
+		OreDictionary.registerOre("plateVibrantAlloy", ModItems.vibrantAlloyPlate);
+	}
+	
+}
