60fc7e033b3e86d5aae0e1837eecae90dcd45b66
PanSzelescik <pan.szelescik20010131@gmail.com>
2017-07-17 17:25:34 +0200

    1.12 version

---
+++ src/main/java/panszelescik/moreplates/creativetabs/TabItems.java
@@ -0,0 +1,19 @@
+package panszelescik.moreplates.creativetabs;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import panszelescik.moreplates.init.ModItems;
+
+public class TabItems extends CreativeTabs{
+
+	public TabItems() {
+		super("items");
+	}
+
+	@Override
+	public ItemStack getTabIconItem() {
+		return new ItemStack(ModItems.arditeGear);
+	}
+
+}
