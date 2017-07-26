53a8866f773408f0c09c269d06e767240bb99be0
PanSzelescik <pan.szelescik20010131@gmail.com>
2017-07-26 18:25:37 +0200

    1.12-2.1.0

--- src/main/java/panszelescik/moreplates/ModChecker.java
+++ src/main/java/panszelescik/moreplates/ModChecker.java
@@ -7,6 +7,7 @@ public class ModChecker {
 	public static boolean isBotaniaLoaded;
 	public static boolean isDraconicEvolutionLoaded;
 	public static boolean isEnderIOLoaded;
+	public static boolean isImmersiveEngineeringLoaded;
 	public static boolean isMekanismLoaded;
 	public static boolean isRefinedStorageLoaded;
 	public static boolean isTinkersConstructLoaded;
@@ -15,6 +16,7 @@ public class ModChecker {
 		this.isBotaniaLoaded = Loader.isModLoaded("botania");
 		this.isDraconicEvolutionLoaded = Loader.isModLoaded("draconicevolution");
 		this.isEnderIOLoaded = Loader.isModLoaded("enderio");
+		this.isImmersiveEngineeringLoaded = Loader.isModLoaded("immersiveengineering");
 		this.isMekanismLoaded = Loader.isModLoaded("mekanism");
 		this.isRefinedStorageLoaded = Loader.isModLoaded("refinedstorage");
 		this.isTinkersConstructLoaded = Loader.isModLoaded("tconstruct");
@@ -22,22 +24,25 @@ public class ModChecker {
 	
 	public static void printSuccessMessage() {
 		if(isBotaniaLoaded) {
-			MorePlates.logger.info("Botania has been detected.");
+			MorePlates.logger.info("Botania has been detected. Loading integration...");
 		}
 		if(isDraconicEvolutionLoaded) {
-			MorePlates.logger.info("Draconic Evolution has been detected.");
+			MorePlates.logger.info("Draconic Evolution has been detected. Loading integration...");
 		}
 		if(isEnderIOLoaded) {
-			MorePlates.logger.info("Ender IO has been detected.");
+			MorePlates.logger.info("Ender IO has been detected. Loading integration...");
+		}
+		if(isImmersiveEngineeringLoaded) {
+			MorePlates.logger.info("Immersive Engineering has been detected. Loading integration...");
 		}
 		if(isMekanismLoaded) {
-			MorePlates.logger.info("Mekanism has been detected.");
+			MorePlates.logger.info("Mekanism has been detected. Loading integration...");
 		}
 		if(isRefinedStorageLoaded) {
-			MorePlates.logger.info("Refined Storage has been detected.");
+			MorePlates.logger.info("Refined Storage has been detected. Loading integration...");
 		}
 		if(isTinkersConstructLoaded) {
-			MorePlates.logger.info("Tinkers' Construct has been detected.");
+			MorePlates.logger.info("Tinkers' Construct has been detected. Loading integration...");
 		}
 	}
 }
