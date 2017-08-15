6bf6bf25773518e3fb99bd915025db89dc45dc48
PanSzelescik <pan.szelescik20010131@gmail.com>
2017-08-15 18:05:00 +0200

    2.2.0
    Removed Immersive Enginnering checking

--- src/main/java/panszelescik/moreplates/ModChecker.java
+++ src/main/java/panszelescik/moreplates/ModChecker.java
@@ -7,7 +7,6 @@ public class ModChecker {
 	public static boolean isBotaniaLoaded;
 	public static boolean isDraconicEvolutionLoaded;
 	public static boolean isEnderIOLoaded;
-	public static boolean isImmersiveEngineeringLoaded;
 	public static boolean isMekanismLoaded;
 	public static boolean isMysticalAgradditionsLoaded;
 	public static boolean isMysticalAgricultureLoaded;
@@ -19,7 +18,6 @@ public class ModChecker {
 		this.isBotaniaLoaded = Loader.isModLoaded("botania");
 		this.isDraconicEvolutionLoaded = Loader.isModLoaded("draconicevolution");
 		this.isEnderIOLoaded = Loader.isModLoaded("enderio");
-		this.isImmersiveEngineeringLoaded = Loader.isModLoaded("immersiveengineering");
 		this.isMekanismLoaded = Loader.isModLoaded("mekanism");
 		this.isMysticalAgradditionsLoaded = Loader.isModLoaded("mysticalagradditions");
 		this.isMysticalAgricultureLoaded = Loader.isModLoaded("mysticalagriculture");
@@ -37,9 +35,6 @@ public class ModChecker {
 		}
 		if(isEnderIOLoaded) {
 			MorePlates.logger.info("Ender IO has been detected. Loading integration...");
-		}
-		if(isImmersiveEngineeringLoaded) {
-			MorePlates.logger.info("Immersive Engineering has been detected. Loading integration...");
 		}
 		if(isMekanismLoaded) {
 			MorePlates.logger.info("Mekanism has been detected. Loading integration...");
