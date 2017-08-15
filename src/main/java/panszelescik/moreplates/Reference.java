7e119a3f8c423e6ee0a735959d87c2e84a85c417
PanSzelescik <pan.szelescik20010131@gmail.com>
2017-08-15 17:40:19 +0200

    2.2.0

--- src/main/java/panszelescik/moreplates/Reference.java
+++ src/main/java/panszelescik/moreplates/Reference.java
@@ -4,8 +4,7 @@ public class Reference {
 
 	public static final String MODID = "moreplates";
 	public static final String NAME = "MorePlates";
-	public static final String VERSION = "1.12-2.2.0.2443";
-	public static final String DEPENDENCIES = "required-after:forge@[14.21.1.2443,);";
+	public static final String VERSION = "1.12.x-2.2.0.2456";
 	public static final String SERVER_PROXY_CLASS = "panszelescik.moreplates.proxy.CommonProxy";
 	public static final String CLIENT_PROXY_CLASS = "panszelescik.moreplates.proxy.ClientProxy";
 	
