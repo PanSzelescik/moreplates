3854585b4532a0ed9dedbb22932cfaf6170694a8
PanSzelescik <pan.szelescik20010131@gmail.com>
2017-09-19 14:59:57 +0200

    Bump to 2.5.1

--- src/main/java/panszelescik/moreplates/Reference.java
+++ src/main/java/panszelescik/moreplates/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
 	public static final String MODID = "moreplates";
 	public static final String NAME = "MorePlates";
-	public static final String VERSION = "2.5.0";
+	public static final String VERSION = "2.5.1";
 	public static final String SERVER_PROXY_CLASS = "panszelescik.moreplates.proxy.CommonProxy";
 	public static final String CLIENT_PROXY_CLASS = "panszelescik.moreplates.proxy.ClientProxy";
 	
