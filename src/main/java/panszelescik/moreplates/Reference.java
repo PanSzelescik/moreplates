8a851a1a52d8fc5527b604e71396d58119c0f3b1
PanSzelescik <pan.szelescik20010131@gmail.com>
2018-01-25 23:17:36 +0100

    Rename

--- src/main/java/panszelescik/moreplates/Reference.java
+++ src/main/java/panszelescik/moreplates/Reference.java
@@ -7,6 +7,6 @@ public class Reference {
 	public static final String VERSION = "2.6.0";
 	public static final String SERVER_PROXY_CLASS = "panszelescik.moreplates.proxy.CommonProxy";
 	public static final String CLIENT_PROXY_CLASS = "panszelescik.moreplates.proxy.ClientProxy";
-	public static final String MINECRAFT_VERSION = "[1.12,1.13)";
+	public static final String ACCEPTED_MINECRAFT_VERSIONS = "[1.12,1.13)";
 	
 }
