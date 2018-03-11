27ec2d98df802f0f3d5f685c4b9927561250d91f
PanSzelescik <pan.szelescik20010131@gmail.com>
2018-03-11 19:31:39 +0100

    2.6.1

--- src/main/java/panszelescik/moreplates/Reference.java
+++ src/main/java/panszelescik/moreplates/Reference.java
@@ -4,7 +4,8 @@ public class Reference {
 
 	public static final String MODID = "moreplates";
 	public static final String NAME = "MorePlates";
-	public static final String VERSION = "2.6.0";
+	public static final String VERSION = "2.6.1";
+	public static final String UPDATE_JSON = "https://raw.githubusercontent.com/PanSzelescik/moreplates/update/update.json";
 	public static final String SERVER_PROXY_CLASS = "panszelescik.moreplates.proxy.CommonProxy";
 	public static final String CLIENT_PROXY_CLASS = "panszelescik.moreplates.proxy.ClientProxy";
 	public static final String ACCEPTED_MINECRAFT_VERSIONS = "[1.10,1.12)";
