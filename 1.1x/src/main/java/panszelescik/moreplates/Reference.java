2c5f43a7df822c705b603b1a9461fd4dcc65bb96
PanSzelescik <pan.szelescik20010131@gmail.com>
2018-03-21 13:59:27 +0100

    Update Forge 1.12.2 to 2631, updated build.gradle and gradle.properties

--- 1.1x/src/main/java/panszelescik/moreplates/Reference.java
+++ 1.1x/src/main/java/panszelescik/moreplates/Reference.java
@@ -11,7 +11,7 @@ public class Reference {
 	public static final String ACCEPTED_MINECRAFT_VERSIONS = "[1.10,1.12)";
 	public static final String FORGE_10_VERSION = "12.18.3.2511";
 	public static final String FORGE_11_VERSION = "13.20.1.2588";
-	public static final String COMPATLAYER_VERSION = "0.3.0";
+	public static final String COMPATLAYER_VERSION = "0.3.1";
 	public static final String DEPENDENCIES = 
 			"required-after:compatlayer@[" + Reference.COMPATLAYER_VERSION + ",);" +
 			"after:Forge@[" + Reference.FORGE_10_VERSION + ",);" +
