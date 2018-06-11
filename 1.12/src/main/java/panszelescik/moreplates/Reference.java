154284b96a76ba185815bfcdd51fd592b1f3d115
PanSzelescik <pan.szelescik20010131@gmail.com>
2018-06-11 19:40:43 +0200

    Fixed update.json

--- 1.12/src/main/java/panszelescik/moreplates/Reference.java
+++ 1.12/src/main/java/panszelescik/moreplates/Reference.java
@@ -5,7 +5,7 @@ public class Reference {
 	public static final String MODID = "moreplates";
 	public static final String NAME = "More Plates";
 	public static final String VERSION = "4.2.0";
-	public static final String UPDATE_JSON = "https://github.com/PanSzelescik/moreplates/blob/master/update.json";
+	public static final String UPDATE_JSON = "https://raw.githubusercontent.com/PanSzelescik/moreplates/master/update.json";
 	public static final String SERVER_PROXY_CLASS = "panszelescik.moreplates.proxy.CommonProxy";
 	public static final String CLIENT_PROXY_CLASS = "panszelescik.moreplates.proxy.ClientProxy";
 	public static final String ACCEPTED_MINECRAFT_VERSIONS = "[1.12.2,1.13)";
