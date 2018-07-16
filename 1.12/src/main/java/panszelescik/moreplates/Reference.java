fe7520c381f591ae644f5cf4d2d785588ec1f2b8
PanSzelescik <pan.szelescik20010131@gmail.com>
2018-07-16 17:07:40 +0200

    Updated dependencies

--- 1.12/src/main/java/panszelescik/moreplates/Reference.java
+++ 1.12/src/main/java/panszelescik/moreplates/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 	
 	public static final String MODID = "moreplates";
 	public static final String NAME = "More Plates";
-	public static final String VERSION = "5.0.1";
+	public static final String VERSION = "@version@";
 	public static final String UPDATE_JSON = "https://raw.githubusercontent.com/PanSzelescik/moreplates/master/update.json";
 	//public static final String SERVER_PROXY_CLASS = "panszelescik.moreplates.proxy.CommonProxy";
 	//public static final String CLIENT_PROXY_CLASS = "panszelescik.moreplates.proxy.ClientProxy";
