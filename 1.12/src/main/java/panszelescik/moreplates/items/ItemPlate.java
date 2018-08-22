84919cd33066621e7daebf42473c98c31a638a36
PanSzelescik <pan.szelescik20010131@gmail.com>
2018-08-22 15:21:21 +0200

    Models --> Blockstates
    .jar file = -35kB (previous commit)

--- 1.12/src/main/java/panszelescik/moreplates/items/ItemPlate.java
+++ 1.12/src/main/java/panszelescik/moreplates/items/ItemPlate.java
@@ -2,7 +2,11 @@ package panszelescik.moreplates.items;
 
 import static panszelescik.moreplates.Reference.*;
 
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ItemPlate extends ItemBase {
 	
@@ -11,5 +15,11 @@ public class ItemPlate extends ItemBase {
 		name += "_plate";
 		setTranslationKey(MODID + "." + name);
 		setRegistryName(new ResourceLocation(MODID, name));
+	}
+	
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModel() {
+		ModelLoader.setCustomModelResourceLocation(this, 0, new ModelResourceLocation(MODID + ":plate", "type=" + getRegistryName().toString().substring(11)));
 	}
 }
